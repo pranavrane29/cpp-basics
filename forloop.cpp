923ab6cb4b6a5b288e862abe4907cec745d27671
Pranav Rane <pranavrane29@gmail.com>
2025-07-06 14:03:01 +0530

    Initial commit with basic C++ programs

---
+++ forloop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 3;
+    for (int i = 0; i < n; n++) // Use i++ to increment the loop variable
+    {
+        cout << "HELLO WORLD" << endl;
+    }
+
+return 0;
+}
