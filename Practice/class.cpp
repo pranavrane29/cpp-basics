95686648d7e472aa2bf2cb13ebb7df4e95713b04
Pranav Rane <pranavrane29@gmail.com>
2025-07-24 16:30:12 +0530

    Initial commit

---
+++ Practice/class.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Students{
+    private:
+        int id;
+        char name[20];
+
+    public:
+        int getdata(){
+            cout<<"Enter Student id - ";
+            cin>>id;
+
+            cout<<"Enter Student name - ";
+            cin>>name;
+ 
+       }
+
+       int displaydata(){
+            cout << "Student id and his Name is -"<<id << endl << name;
+       }
+    
+};
+
+    int main(){
+        Students s1,s2,s3;
+
+        s1.getdata();
+        s2.getdata();
+        s3.getdata();
+
+        s1.displaydata();
+        s2.displaydata();
+        s3.displaydata();
+
+    return 0;
+    }
