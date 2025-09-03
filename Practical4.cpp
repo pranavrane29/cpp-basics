7e4fe350c63d6bda55530cb9a8bc6632e8968003
Pranav Rane <pranavrane29@gmail.com>
2025-09-03 12:06:13 +0530

    Initial commit

---
+++ Practical4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class teacher
+{
+private:
+    int salary;
+    char name[10];
+public:
+  void accept()
+  {
+      cout<<"Enter name and salary of teacher"<<endl;
+      cin>>name>>salary;
+  }
+
+  void display()
+  {
+      cout<<"Name of teacher is: "<<name<<endl;
+      cout<<"Salary of teacher is: "<<salary<<endl;
+  }
+};
+
+int main()
+{
+    teacher t1;
+    t1.accept();
+    t1.display();
+    return 0;
+}
